215c4aebdab220b727c37d5eadfcf9118542d2a6
PC_SysakA\Andriy <andriy.sysak@gmail.com>
2018-04-07 19:07:47 +0300

    Initial state

---
+++ U7MVC/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace U7MVC.Models
+{
+    public class Users
+    {
+        
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+        public int UserTypeId { get; set; }
+        public bool isActive { get; set; }
+    }
+}
