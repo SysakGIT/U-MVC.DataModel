9a06b69a03d046141f8090e59101bbfb6ce13b6a
AndriySysak <andriy.sysak@eleks.com>
2018-04-12 22:10:11 +0300

    added table name

--- U7MVC/Models/Region.cs
+++ U7MVC/Models/Region.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace U7MVC.Models
 {
+    [Table("Regions")]
     public class Region
     {
         public int ID { get; set; }
